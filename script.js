d06830849ee65405c15101d3e7c03d8113ea7a32
vibovenkat123 <vibovenkat@outlook.com>
2022-07-15 16:35:27 -0500

    added game loop

--- script.js
+++ script.js
@@ -7,15 +7,20 @@ const onClick = (cell, xClass, oClass, isOTurn, hoverX, hoverO) => {
   switchTurns();
   onHover(hoverX,  hoverO)
 }
+function onStart(hoverClassX, hoverClassO, classX, classO){
+  oTurn = false;
+  onHover(hoverClassX, hoverClassO)
+  cells.forEach(cell => {
+    cell.addEventListener('click', function(e){
+      let cell = e.target;
+      onClick(cell, classX, classO, oTurn, hoverClassX, hoverClassO)
+    }, {once: true});
+  });
+}
 function drawLetter(cell, currentClass){
   cell.classList.add(currentClass)
 }
-cells.forEach(cell => {
-  cell.addEventListener('click', function(e){
-    let cell = e.target;
-    onClick(cell, 'letter-x', 'letter-o', oTurn, 'x-active', 'o-active')
-  }, {once: true});
-});
+
 function onHover(classX, classO){
   board.classList.remove(classX);
   board.classList.remove(classO);
@@ -29,3 +34,4 @@ function onHover(classX, classO){
 function switchTurns(){
   oTurn = !oTurn;
 }
+onStart('x-active', 'o-active', 'letter-x', 'letter-o');
